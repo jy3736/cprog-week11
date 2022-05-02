2bd52fc26a8e0ce9dd2dc9c3423af57a59696e28
jy3736 <jyang@stust.edu.tw>
2022-05-03 01:01:10 +0800

    jyang

--- src/hw01/main.cpp
+++ src/hw01/main.cpp
@@ -3,36 +3,7 @@
 
 using namespace std;
 
-void printAll(int n)
-{
-    cout << "All     : ";
-    for (int i = 1; i <= n; i++)
-        cout << setw(4) << i;
-}
-
-void printEven(int n)
-{
-    cout << "Even    : ";
-    for (int i = 1; i <= n; i++)
-        if (i % 2 == 0)
-            cout << setw(4) << i;
-}
-
-void printOdd(int n)
-{
-    cout << "Odd     : ";
-    for (int i = 1; i <= n; i++)
-        if (i % 2 != 0)
-            cout << setw(4) << i;
-}
-
-void printDiv4(int n)
-{
-    cout << "Div. 4  : ";
-    for (int i = 1; i <= n; i++)
-        if (i % 4 == 0)
-            cout << setw(4) << i;
-}
+// add your code here
 
 // -----vv----- 不得修改『以下』的程式 -----vv-----
 
